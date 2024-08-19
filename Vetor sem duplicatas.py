b3da51df90c9d2b6de49db03ad09f5641b760406
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2024-08-18 23:30:10 -0300

    Vetor sem duplicatas.py

---
+++ Vetor sem duplicatas.py
@@ -0,0 +1,12 @@
+entrada = input("Digite os elementos do vetor: ")
+
+vetor = []  
+for elemento in entrada.split():  
+    vetor.append(int(elemento)) 
+
+sem_duplicatas = []
+for elemento in vetor:
+    if elemento not in sem_duplicatas:
+        sem_duplicatas.append(elemento)
+
+print("Vetor sem duplicatas:", sem_duplicatas)
