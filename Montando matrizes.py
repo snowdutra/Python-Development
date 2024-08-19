9ea99d44848999a726432f22f88b81277e2ef2fd
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2024-08-18 23:31:29 -0300

    Montador de matriz.py

---
+++ Montando matrizes.py
@@ -0,0 +1,28 @@
+matriz = []
+
+ordem = int(input("Insira a ordem da matriz: "))
+
+print("Insir o valor da matriz:")
+
+for i in range(ordem):
+    linha = []
+    for k in range(ordem):
+        valor = int(input(f"Digite o valor da posição {k}: "))
+        linha.append(valor)
+    matriz.append(linha)
+
+
+print("\n Matriz:")
+for linha in matriz:
+    print(linha)
+
+diagonal_principal = 0
+for i in range(ordem):
+    diagonal_principal += matriz[i][i]
+
+diagonal_secundaria = 0
+for i in range(ordem):
+    diagonal_secundaria += matriz[i][ordem - 1 - i]
+
+print(f"\n A Soma dos elementos da diagonal principal é igual a: {diagonal_principal}")
+print(f"\n A Soma dos elementos da diagonal secundária é igual a: {diagonal_secundaria}")
