3a6c5505b386e65ac19166c8acdfb5fa3e741256
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2024-10-28 08:04:29 -0300

    Create Transações.py

---
+++ Transações.py
@@ -0,0 +1,20 @@
+
+def bubble_sort(transacoes):
+    n = len(transacoes)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if transacoes[j]['saldo'] > transacoes[j+1]['saldo']:
+                transacoes[j], transacoes[j+1] = transacoes[j+1], transacoes[j]
+
+def nome_e_saldo(transacao_bancarias):
+    bubble_sort(transacao_bancarias)
+    for i in range(len(transacao_bancarias)):
+        print(transacao_bancarias[i]['nome'], transacao_bancarias[i]['saldo'])
+
+trasacoes_bancarias = [
+    { 'nome': 'João', 'saldo': 1000},
+    { 'nome': 'Maria', 'saldo': 1500},
+    { 'nome': 'Pedro', 'saldo': 2000}
+]
+
+nome_e_saldo(trasacoes_bancarias)
